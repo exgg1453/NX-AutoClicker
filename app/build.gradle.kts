519215ea97268b736238f2d1094117551df5134d
NX-developer <exgg1453@users.noreply.github.com>
2026-08-17 03:02:35 +0000

    Disable fatal lint on release build for expired targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,11 @@ android {
         }
     }
 
+    lint {
+        checkReleaseBuilds = false
+        abortOnError = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
